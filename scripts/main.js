d5d6b460739d7e13bbd28bde8f6803a8f0b7fea1
“Vladyslav.Fedoruk” <mr.fedoruk17@gmail.com>
2023-08-13 16:08:58 +0300

    house

--- scripts/main.js
+++ scripts/main.js
@@ -20,11 +20,29 @@ class Flat {
 }
 const flat = new Flat();
 
-
 flat.addInhabitant(person1);
 flat.addInhabitant(person2);
 flat.addInhabitant(person3);
 
 console.log(flat.community);
+class House {
+    constructor(maxFlats) {
+        this.flats = [];
+        this.maxFlats = maxFlats;
+    }
+    
+    addFlat(flat) {
+        if (this.flats.length < this.maxFlats) {
+            this.flats.push(flat);
+        } else {
+            console.log("Максимальна кількість квартир досягнута");
+        }
+    }
+}
+
+const house = new House(6); 
+house.addFlat(flat);
+
+console.log(house.flats);
 
 
